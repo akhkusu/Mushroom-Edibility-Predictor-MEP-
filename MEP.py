ac1f11c0c77e3c1d5b140ff0732256d45b62df16
akhkusu <125063574+akhkusu@users.noreply.github.com>
2023-10-20 00:28:00 +0300

    Update MEP.py

--- MEP.py
+++ MEP.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
 
 
 
